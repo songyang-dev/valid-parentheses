2f99ca850f71020b9c285f74ec73724b0362a1f7
Song Yang <songyang.dev@gmail.com>
2023-03-19 21:27:34 -0400

    Supply javadoc comments

--- app/src/main/java/validparentheses/Preprocessor.java
+++ app/src/main/java/validparentheses/Preprocessor.java
@@ -3,8 +3,19 @@ package validparentheses;
 import java.util.Arrays;
 import java.util.List;
 
+/**
+ * Class that processes the input strings received from the command line
+ */
 public class Preprocessor {
 
+  /**
+   * Preprocesses the {@code rawInput} by removing any characters that are
+   * not opening or closing brackets such as {@code (, [} and curly braces.
+   * 
+   * @param rawInput Any string, but it cannot be null.
+   * 
+   * @return String containing only brackets
+   */
   public String preprocess(String rawInput) {
     List<Character> acceptableBrackets = Arrays.asList('(', ')', '[', ']', '{', '}');
 
